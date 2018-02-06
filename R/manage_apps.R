63af473c7645766afbeca09a3e71ebffd5475fb4
cvarelam <cvarelamail@gmail.com>
2018-02-06 16:24:57 +0100

    initial commit

---
+++ R/manage_apps.R
@@ -0,0 +1,15 @@
+# Manage apps
+
+deploy_app <- function(folder, user = "ubuntu") {
+  command <- sprintf("sudo cp -R /home/%s/%s /srv/shiny-server",
+                     user,
+                     folder)
+  system(command)
+
+}
+
+remove_app <- function(app) {
+  command <- sprintf("sudo rm -r /../srv/shiny-server/%s",
+                     app)
+  system(command)
+}
